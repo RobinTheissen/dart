ae7373abebfbb1399a8ccc1bb6b45efeb483c7da
RobinTheissen <robin.theissen00@gmail.com>
2024-05-15 22:49:12 +0200

    =Spiel kann richtig beendet werden

--- X01/X01-game/X01-game.js
+++ X01/X01-game/X01-game.js
@@ -8,6 +8,17 @@ function handleFormSubmission(event) {
     event.preventDefault();
 }
 
+function throwHandler(){
+    if (throws === 3) {
+        throws = 1; // Reset für neue Eingaben
+        document.querySelector('.firstThrow').innerHTML = ``;
+        document.querySelector('.secondThrow').innerHTML = ``;
+        document.querySelector('.thirdThrow').innerHTML = ``;
+    } else {
+        throws++;
+    }
+}
+
 // Funktion zum Aktualisieren der Anzeige der Würfe
 function updateThrowsDisplay(points) {
     if (throws === 1){
@@ -26,6 +37,10 @@ function updateRestScore() {
         document.querySelector('.restScore').innerHTML = `Rest: ${selectedOptions.points}`;
     } else if (selectedOptions.points === 0){
         document.querySelector('.calculatedPoints').innerHTML = `Gewonnen`;
+        document.querySelector('.restScore').innerHTML = ``;
+        document.querySelector('.firstThrow').innerHTML = ``;
+        document.querySelector('.secondThrow').innerHTML = ``;
+        document.querySelector('.thirdThrow').innerHTML = ``;
     }
 }
 
@@ -53,62 +68,123 @@ function calculatePoints(points) {
 
     document.getElementById('scoreboardForm').addEventListener('submit', handleFormSubmission);
 
-    changeMode(points);
+    let doubledPoints = points * 2
+    let tripledPoints = points * 3
 
+    changeMode(points);
+    
     if (selectedOptions.points - points > 0) {
-        if(doubleMode === true && tripleMode === false){
-            points = points * 2
+        if(doubleMode === true && selectedOptions.points - doubledPoints > 0){
+            throwHandler();
+            points = doubledPoints
             totalPoints += points
             selectedOptions.points -= points
             localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
             console.log(selectedOptions);
             doubleMode = false
             tripleMode = false
+            console.log(throws)
+            updateThrowsDisplay(points);
+            console.log(points)
+            console.log(doubledPoints)
+            console.log(tripledPoints)
+            console.log(selectedOptions.points)
+            console.log('1')
         }
-        else if(tripleMode === true && doubleMode === false){
-            points = points * 3
+        else if(tripleMode === true && selectedOptions.points - tripledPoints > 0){
+            throwHandler();
+            points = tripledPoints
             totalPoints += points
             selectedOptions.points -= points
             localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
             console.log(selectedOptions);
             tripleMode = false
             doubleMode = false
+            console.log(throws)
+            updateThrowsDisplay(points);
+            console.log(points)
+            console.log(doubledPoints)
+            console.log(tripledPoints)
+            console.log(selectedOptions.points)
+            console.log('2')
         }
-        else if(tripleMode === false && doubleMode === false){
+        else if(selectedOptions.points - points > 0){
+            throwHandler();
             totalPoints += points
             selectedOptions.points -= points
             localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
             console.log(selectedOptions);
             tripleMode = false
             doubleMode = false
+            console.log(throws)
+            updateThrowsDisplay(points);
+            console.log(points)
+            console.log(doubledPoints)
+            console.log(tripledPoints)
+            console.log(selectedOptions.points)
+            console.log('3')
         }
         else{
-            console.log('Triple SB nicht möglich!')
+            console.log('error bei restpunkten')
             doubleMode = false
             tripleMode = false
+            console.log(throws)
         }
         
-        if (throws === 3) {
-            throws = 1; // Reset für neue Eingaben
-            document.querySelector('.firstThrow').innerHTML = ``;
-            document.querySelector('.secondThrow').innerHTML = ``;
-            document.querySelector('.thirdThrow').innerHTML = ``;
-            totalPoints = points;
-        } else {
-            throws++;
-        }
-    } else if(selectedOptions.points - points === 0) {
+       
+    }else if(doubleMode === true && selectedOptions.points - doubledPoints === 0) {
+        points = doubledPoints
+        totalPoints += points
         selectedOptions.points -= points
-        document.querySelector('.restScore').innerHTML = `Rest: ${selectedOptions.points}`;
         localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
-        document.querySelector('.firstThrow').innerHTML = ``;
-        document.querySelector('.secondThrow').innerHTML = ``;
-        document.querySelector('.thirdThrow').innerHTML = ``;
         console.log(selectedOptions);
-        console.log('Gewonnen');
+        doubleMode = false
+        tripleMode = false
+        updateThrowsDisplay(points);
+        console.log(points)
+            console.log(doubledPoints)
+            console.log(tripledPoints)
+            console.log(selectedOptions.points)
+            console.log('4')
+    }
+    else if(tripleMode === true && selectedOptions.points - tripledPoints === 0){
+        points = tripledPoints
+        totalPoints += points
+        selectedOptions.points -= points
+        localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
+        console.log(selectedOptions);
+        tripleMode = false
+        doubleMode = false
+        updateThrowsDisplay(points);
+        console.log(points)
+            console.log(doubledPoints)
+            console.log(tripledPoints)
+            console.log(selectedOptions.points)
+            console.log('5')
+    }
+    else if(selectedOptions.points - points === 0){
+        totalPoints += points
+        selectedOptions.points -= points
+        localStorage.setItem('selectedOptions', JSON.stringify(selectedOptions));
+        console.log(selectedOptions);
+        tripleMode = false
+        doubleMode = false
+        updateThrowsDisplay(points);
+        console.log(points)
+            console.log(doubledPoints)
+            console.log(tripledPoints)
+            console.log(selectedOptions.points)
+            console.log('6')
+        }
+    else{
+        console.log('überworfen')
+        doubleMode = false
+        tripleMode = false
+        updateThrowsDisplay(points);
+        console.log(throws)
     }
 
-    updateThrowsDisplay(points);
+    
     updateRestScore();
 }
 
